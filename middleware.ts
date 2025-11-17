b9f4062632f064310dc76bce0808718d72e057c7
romuloh21 <romulo.pereira21@edu.udesc.br>
2025-11-17 09:21:54 -0300

    Corrige setup do Drizzle, Neon e Clerk

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes:['/']
+  // Esta linha diz ao Clerk que a página inicial "/" é pública
+  // e não precisa de login. É isso que quebra o loop.
+  publicRoutes: ["/"],
 });
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
